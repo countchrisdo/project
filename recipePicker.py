a8b0c3a91fec408b6e200f6ced8884ca71bc9f2e
countchrisdo <crburwell@yahoo.com>
2024-07-10 11:51:33 -0400

    first commit

---
+++ recipePicker.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+# initiallize window opject we are calling loop
+root = tk.Tk()
+
+# call mainloop method that displays our window untill we hit the Exit button
+root.mainloop()
+
