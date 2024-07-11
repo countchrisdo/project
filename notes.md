de34c65d926dfcbc09b4f7699cd71bf7230652fd
countchrisdo <crburwell@yahoo.com>
2024-07-11 15:00:12 -0400

    Importing tkmacosx

--- notes.md
+++ notes.md
@@ -7,3 +7,8 @@
     - pyglet
 
 - To run the project, run `python main.py`.
+
+# Mac OS Information
+- There are Mac OS specific issues with tkinter. 
+- Using tkmacosx module for tkinter buttons in mac
+    -  ```from tkmacosx import Button``` instead of ```from tkinter import Button```
--- recipePicker.py
+++ recipePicker.py
@@ -1,14 +1,23 @@
+"""Recipe Picker"""
 import tkinter as tk
 from PIL import ImageTk
+import tkmacosx as tkx
+from tkmacosx import Button
 
-#variables
+
+# variables
 bg_color = "#3d6466"
+
+# functions
+def load_frame2():
+    """Load frame2 when button is clicked"""
+    print("Hello from frame2")
 
 # initiallize window opject we are calling loop
 root = tk.Tk()
 root.title("Recipe Picker")
 # eval method is used to execute a command in the tcl interpreter
-root.eval('tk::PlaceWindow . center')
+root.eval("tk::PlaceWindow . center")
 
 # create a frame widget
 frame1 = tk.Frame(root, width=500, height=600, bg=bg_color)
@@ -21,13 +30,26 @@ logo_widget = tk.Label(frame1, image=logo_img, bg=bg_color)
 logo_widget.image = logo_img
 logo_widget.pack()
 
-tk.Label(frame1,
-         text="ready for your random recipe?",
-         bg=bg_color,
-         fg="white",
-         font=("TkMenuFont", 14)
-        ).pack()
+tk.Label(
+    frame1,
+    text="ready for your random recipe?",
+    bg=bg_color,
+    fg="white",
+    font=("TkMenuFont", 14),
+).pack()
+
+# button widget
+tkx.Button(
+    frame1,
+    text="Pick a Recipe",
+    font=("TkHeadingFont", 20),
+    bg="#28393a",
+    fg="white",
+    cursor="hand2",
+    activebackground="#badee2",
+    activeforeground="black",
+    command=load_frame2(),
+).pack()
 
 # call mainloop method that displays our window untill we hit the Exit button
 root.mainloop()
-
